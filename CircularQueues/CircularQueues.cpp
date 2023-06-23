e44812cc163f0bf528a66b572454af314a804a5c
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2023-06-23 22:15:29 +0700

    Iterasi dari awal array hingga REAR

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -80,5 +80,21 @@ public:
 			}
 			cout << endl;
 		}
+		else {
+			// Jika FRONT_position > REAR_position, iterasi dari FRONT hingg akhir array
+			while (FRONT_position <= max - 1) {
+				cout << queue_array[FRONT_position] << "   ";
+				FRONT_position++;
+			}
+
+			FRONT_position = 0;
+
+			// Iterasi dari awal array hingga REAR
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << "   ";
+				FRONT_position++;
+			}
+			cout << endl;
+		}
 	}
 };
