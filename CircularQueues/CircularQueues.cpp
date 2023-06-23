ed75a71aac964571ef62ba11eae8de282d76e0ee
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2023-06-23 22:12:23 +0700

    mengecek apakah antrian kosong

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -61,5 +61,15 @@ public:
 	}
 
 	void display() {
+		int FRONT_position = FRONT;
+		int REAR_position = REAR;
+
+		// Cek apakah antrian kosong
+		if (FRONT == -1) {
+			cout << "Queue is empty\n";
+			return;
+		}
+
+		cout << "\nElement in the queue are: \n";
 	}
 };
